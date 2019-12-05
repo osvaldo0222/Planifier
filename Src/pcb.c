3298d93f6fd37e0ccea080a7712811cea58935b3
Osvaldo Fernández <osvaldof22@hotmail.com>
2019-12-04 20:59:57 -0400

    Planifier

---
+++ Src/pcb.c
@@ -0,0 +1,108 @@
+/*
+ * planificador.c
+ *
+ *  Created on: Dec 4, 2019
+ *      Author: osvaldofdez
+ */
+
+#include "main.h"
+#include "pcb.h"
+
+Procces proccess[N];
+int current_proccess = 0;
+int users_proccess = 0;
+int counter = 0;
+
+int fork(){
+	asm("SVC 0");
+	return proccess[current_proccess].id;
+}
+
+void sem_init(sem_t* semaphore, uint32_t value){
+	*semaphore = value;
+}
+
+void sem_wait(sem_t* semaphore) {
+	while(*semaphore <= 0){
+		continue;
+	}
+	*semaphore = *semaphore - 1;
+}
+
+void sem_post(sem_t* semaphore) {
+	*semaphore = *semaphore + 1;
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void) {
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+	users_proccess++;
+	proccess[users_proccess].id = users_proccess;
+	register int p1 asm("r0");
+	__asm("TST lr, #4");
+	__asm("MRS r0, MSP;");
+	stack_pointer = (int*)p1;
+	stack_pointer += 4;
+	for(int i = 0;i < 8;i++) {
+		proccess[users_proccess].core_registers[i] = stack_pointer[i];
+	}
+	__asm("MOV %0, r4" : "=r" (proccess[users_proccess].user_registers[0]));
+	__asm("MOV %0, r5" : "=r" (proccess[users_proccess].user_registers[1]));
+	__asm("MOV %0, r6" : "=r" (proccess[users_proccess].user_registers[2]));
+	__asm("MOV %0, r7" : "=r" (proccess[users_proccess].user_registers[3]));
+	__asm("MOV %0, r8" : "=r" (proccess[users_proccess].user_registers[4]));
+	__asm("MOV %0, r9" : "=r" (proccess[users_proccess].user_registers[5]));
+	__asm("MOV %0, r10" : "=r" (proccess[users_proccess].user_registers[6]));
+	__asm("MOV %0, r11" : "=r" (proccess[users_proccess].user_registers[7]));
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void) {
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+	counter++;
+	if(users_proccess > 0) {
+		counter = 0;
+		register int p1 asm("r0");
+		__asm("TST lr, #4");
+		__asm("MRS r0, MSP;");
+		stack_pointer = (int*)p1;
+		stack_pointer += 4;
+		for(int i = 0;i < 8;i++) {
+			proccess[current_proccess].core_registers[i] = stack_pointer[i];
+		}
+		__asm("MOV %0, r4" : "=r" (proccess[current_proccess].user_registers[0]));
+		__asm("MOV %0, r5" : "=r" (proccess[current_proccess].user_registers[1]));
+		__asm("MOV %0, r6" : "=r" (proccess[current_proccess].user_registers[2]));
+		__asm("MOV %0, r7" : "=r" (proccess[current_proccess].user_registers[3]));
+		__asm("MOV %0, r8" : "=r" (proccess[current_proccess].user_registers[4]));
+		__asm("MOV %0, r9" : "=r" (proccess[current_proccess].user_registers[5]));
+		__asm("MOV %0, r10" : "=r" (proccess[current_proccess].user_registers[6]));
+		__asm("MOV %0, r11" : "=r" (proccess[current_proccess].user_registers[7]));
+
+		current_proccess = (current_proccess == 0)?1:0;
+
+		for (int i = 0;i < 8;i++) {
+			stack_pointer[i] = proccess[current_proccess].core_registers[i];
+		}
+		__asm("MOV r4, %0" :: "r" (proccess[current_proccess].user_registers[0]));
+		__asm("MOV r5, %0" :: "r" (proccess[current_proccess].user_registers[1]));
+		__asm("MOV r6, %0" :: "r" (proccess[current_proccess].user_registers[2]));
+		__asm("MOV r8, %0" :: "r" (proccess[current_proccess].user_registers[4]));
+		__asm("MOV r9, %0" :: "r" (proccess[current_proccess].user_registers[5]));
+		__asm("MOV r10, %0" :: "r" (proccess[current_proccess].user_registers[6]));
+		__asm("MOV r11, %0" :: "r" (proccess[current_proccess].user_registers[7]));
+	}
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
---
+++ Src/pcb.h
@@ -0,0 +1,26 @@
+/*
+ * pcb.h
+ *
+ *  Created on: Nov 22, 2019
+ *      Author: osvaldofdez
+ */
+
+#ifndef PCB_H_
+#define PCB_H_
+
+#define N 2
+
+typedef struct {
+	int id;
+	int core_registers[8]; //r0-r3,r12,lr,pc,xpsr
+	int user_registers[8]; //r4-r11
+}Procces;
+
+typedef int sem_t;
+int* stack_pointer;
+int fork();
+void sem_wait(sem_t* semaphore);
+void sem_post(sem_t* semaphore);
+void sem_init(sem_t* semaphore, uint32_t value);
+
+#endif /* PCB_H_ */
